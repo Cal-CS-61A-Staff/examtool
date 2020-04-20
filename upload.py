f43606ef705e6b0a22bfd70f70ecfa9d45c016e4
rahularya <rahularya@berkeley.edu>
2020-04-20 14:28:03 +0800

    feat: improved upload.py to run faster with a better CLI

--- upload.py
+++ upload.py
@@ -1,34 +1,47 @@
 import csv
 import json
 
+import click
 from cryptography.fernet import Fernet
 from google.cloud import firestore
 
 
-def upload_exam(exam_name, exam, roster):
+@click.command()
+@click.option("--name", prompt=True, default="cs61a-test-final")
+@click.option("--exam", prompt=True, default="sample_exam.json", type=click.File('r'))
+@click.option("--roster", prompt=True, default="sample_roster.csv", type=click.File('r'))
+def upload_exam(name, exam, roster):
+    roster = csv.reader(roster, delimiter=',')
+
     db = firestore.Client()
-    ref = db.collection("exams").document(exam_name)
+    ref = db.collection("exams").document(name)
     exam = json.loads(exam)
     exam["secret"] = Fernet.generate_key()
     ref.set(exam)
 
-    ref = db.collection("roster").document(exam_name).collection("deadline")
+    ref = db.collection("roster").document(name).collection("deadline")
     next(roster)  # ditch headers
+    batch = db.batch()
+    cnt = 0
     for email, deadline in roster:
-        ref.document(email).set({"deadline": int(deadline)})
+        doc_ref = ref.document(email)
+        batch.set(doc_ref, {"deadline": int(deadline)})
+        cnt += 1
+        if cnt > 400:
+            batch.commit()
+            batch = db.batch()
+            cnt = 0
+            print("Batch of 400 writes complete")
+    batch.commit()
 
     ref = db.collection("exams").document("all")
     data = ref.get().to_dict()
-    if exam_name not in data["exam-list"]:
-        data["exam-list"].append(exam_name)
+    if name not in data["exam-list"]:
+        data["exam-list"].append(name)
     ref.set(data)
 
     print("Exam uploaded with password:", exam["secret"][:-1].decode("utf-8"))
 
 
 if __name__ == '__main__':
-    exam_name = input("Exam Name: [cs61a-exam-test]? ") or "cs61a-exam-test"
-    exam_json = input("Exam JSON: [sample_exam.json]? ") or "sample_exam.json"
-    roster_csv = input("Roster CSV: [sample_roster.csv]? ") or "sample_roster.csv"
-    with open(exam_json) as f, open(roster_csv) as g:
-        upload_exam(exam_name, f.read(), csv.reader(g, delimiter=','))
+    upload_exam()
