8b607841158b418500d11bd034390d8726a25cd7
rahularya <rahularya@berkeley.edu>
2020-05-05 09:43:16 -0700

    fix: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="examtool",
-    version="0.1.0",
+    version="0.1.1",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     licence="MIT",
