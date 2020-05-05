e94a3f72b58df8f1bfbb3b40b53bd63c2b3a878a
rahularya <rahularya@berkeley.edu>
2020-05-05 12:20:12 -0700

    build: bump to release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="0.2.1",
+    version="1.0.0",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
