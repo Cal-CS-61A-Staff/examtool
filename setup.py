6e67c44132b3b8ed45bf32c2144f9819a1a74545
rahularya <rahularya@berkeley.edu>
2020-05-05 10:34:04 -0700

    fix: make CLI work again

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="examtool",
-    version="0.1.6",
+    version="0.1.7",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     licence="MIT",
     packages=find_packages(include=["examtool.api", "examtool.cli"]),
     entry_points={
         "console_scripts": [
-            "examtool=cli.__main__:cli",
+            "examtool=examtool.cli.__main__:cli",
         ]
     }, install_requires=['click', 'pikepdf', 'pytz', 'requests', 'fpdf', 'cryptography']
 )
