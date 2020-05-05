fc880f904c82dff38b0beecec4c59850b69b7824
rahularya <rahularya@berkeley.edu>
2020-05-05 13:00:15 -0700

    build: simplify deps to make it work on admin gcloud

--- setup.py
+++ setup.py
@@ -6,21 +6,18 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="1.0.0",
+    version="1.0.1",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
     long_description_content_type="text/markdown",
     licence="MIT",
     packages=find_packages(include=["examtool.api", "examtool.cli"]),
-    entry_points={
-        "console_scripts": [
-            "examtool=examtool.cli.__main__:cli",
-        ]
-    },
-    python_requires='>=3.6',
-    install_requires=['cryptography'],
+    entry_points={"console_scripts": ["examtool=examtool.cli.__main__:cli"]},
+    python_requires=">=3.6",
+    install_requires=["cryptography"],
     extras_require={
-        "cli": ['pikepdf', 'pytz', 'requests', 'fpdf', 'pypandoc']
-    }
+        "admin": ["pytz", "requests", "pypandoc"],
+        "cli": ["pikepdf", "pytz", "requests", "fpdf", "pypandoc"],
+    },
 )
