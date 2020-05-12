da317fa3bd39948f08256650931cceb81ca54f9c
Stephan K <thaumicmekanism@gmail.com>
2020-05-12 11:32:01 -0700

    Added compile flag to locally compile a md file and added scrambling (#4)

--- examtool/cli/compile.py
+++ examtool/cli/compile.py
@@ -1,13 +1,15 @@
 import os
 import pathlib
 from io import BytesIO
-from json import load
+from json import load, dump
 
 import click
 from pikepdf import Pdf
 
+from examtool.api.convert import convert
 from examtool.api.database import get_exam
 from examtool.api.gen_latex import render_latex
+from examtool.api.scramble import scramble
 from examtool.cli.utils import exam_name_option, hidden_output_folder_option, prettify
 
 
@@ -19,22 +21,56 @@ from examtool.cli.utils import exam_name_option, hidden_output_folder_option, pr
     type=click.File("r"),
     help="The exam JSON you wish to compile. Leave blank to compile the deployed exam.",
 )
+@click.option(
+    "--md",
+    default=None,
+    type=click.File("r"),
+    help="The exam Markdown you wish to compile. Leave blank to compile the deployed exam.",
+)
+@click.option(
+    "--seed",
+    default=None,
+    help="Scrambles the exam based off of the seed (E.g. a student's email)."
+)
+@click.option(
+    "--json-out",
+    default=None,
+    type=click.File("w"),
+    help="Exports the JSON to the file specified."
+)
 @hidden_output_folder_option
-def compile(exam, json, out):
+def compile(exam, json, md, seed, json_out, out):
     """
-    Compile one PDF, unencrypted.
-    Exam must have been deployed first.
+    Compile one PDF or JSON (from Markdown), unencrypted.
+    The exam may be deployed or local (in Markdown or JSON).
+    If a seed is specified, it will scramble the exam.
     """
     if not out:
         out = ""
 
     pathlib.Path(out).mkdir(parents=True, exist_ok=True)
 
-    if not json:
-        exam_data = get_exam(exam=exam)
-    else:
+    if json:
+        print("Loading exam...")
         exam_data = load(json)
+    elif md:
+        print("Compiling exam...")
+        exam_text_data = md.read()
+        exam_data = convert(exam_text_data)
+    else:
+        print("Fetching exam...")
+        exam_data = get_exam(exam=exam)
 
+    if seed:
+        print("Scrambling exam...")
+        exam_data = scramble(seed, exam_data,)
+
+    if json_out:
+        print("Dumping json...")
+        dump(exam_data, json_out)
+        return
+
+    print("Rendering exam...")
     with render_latex(
         exam_data, {"coursecode": prettify(exam.split("-")[0]), "description": "Sample Exam."}
     ) as pdf:
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="1.2.6",
+    version="1.2.7",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
