5068a2c9bc1c2ebf1a9bd87f08e93c2ad493327f
rahularya <rahularya@berkeley.edu>
2020-05-15 13:48:32 -0700

    build: force redeploy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="1.5.0",
+    version="1.5.1",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
