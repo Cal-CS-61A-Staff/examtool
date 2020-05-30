ae2971ef071a2b442313e94cd2a515c371251290
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-05-30 01:18:21 -0700

    Checkpoint on prog

--- examtool/api/gradescope_autograde.py
+++ examtool/api/gradescope_autograde.py
@@ -1,9 +1,10 @@
 import examtool.api.download
 from examtool.api.gradescope_upload import APIClient
 from examtool.api.fullGSapi.client import GradescopeClient
-from examtool.api.fullGSapi.assignment_grader import GS_Crop_info, GS_Outline, GS_assignment_Grader, GS_Outline_Question
+from examtool.api.fullGSapi.assignment_grader import GS_Crop_info, GS_Outline, GS_assignment_Grader, GS_Outline_Question, GS_Question, GroupTypes
 from examtool.api.extract_questions import extract_groups, extract_questions, extract_public
 import os
+import time
 
 class GradescopeGrader:
     def __init__(self, email: str=None, password: str=None, gs_client: GradescopeClient=None, gs_api_client: APIClient=None):
@@ -59,10 +60,20 @@ class GradescopeGrader:
         # We can now upload the student submission since we have an outline
         self.upload_student_submissions(out, gs_class_id, gs_assignment_id)
 
-        # TODO Add rubrics
-        # TODO Group questions
-        # in here, add check to see if qid is equal to either name or sid q id so we do not group those.
-        # TODO Grade questions
+        # TODO For each question, group, add rubric and grade
+        print("Setting the grade type for grouping for each question....")
+        gs_outline = examtool_outline.get_gs_outline()
+        self.set_group_types(gs_outline)
+
+        # Fetch the student email to question id map
+        print("Fetching the student email to submission id's mapping....")
+        email_to_question_sub_id = grader.student_email_to_submission_ids()
+
+        # Finally we can process each question
+        print("Grouping and grading questions...")
+        for qid, question in gs_outline.questions_iterator():
+            print(f"Processing question {qid}...")
+            self.process_question(qid, question, email_to_responses_map, email_to_question_sub_id)
 
 
     def export_exam(self, template_questions, email_to_data_map, total, exam, out, name_question_id, sid_question_id):
@@ -90,6 +101,89 @@ class GradescopeGrader:
                 continue
             student_email = file_name[:-4]
             self.gs_api_client.upload_submission(gs_class_id, assignment_id, student_email, os.path.join(out, file_name))
+
+    def set_group_types(self, outline: GS_Outline, debug=True):
+        for qid, question in outline.questions_iterator():
+            self.set_group_type(question)
+    
+    def set_group_type(self, o_question: GS_Outline_Question):
+        question_type = o_question.data.get("type")
+        q = o_question.get_gs_question()
+        q_type = GroupTypes.complex
+        if question_type in ["select_all", "multiple_choice"]:
+            q_type = GroupTypes.mc
+        if question_type in ["long_answer", "long_code_answer"]:
+            q_type = GroupTypes.non_grouped
+        return q.set_group_type(q_type)
+    
+    def process_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        # TODO Group questions
+        groups = self.group_question(question, num_id, email_to_responses_map, email_to_question_sub_id_map)
+        if groups:
+            # TODO Add rubrics
+            self.add_rubric(question, email_to_responses_map, email_to_question_sub_id_map)
+            # in here, add check to see if qid is equal to either name or sid q id so we do not group those.
+            # TODO Grade questions
+            self.grade_question(question, email_to_responses_map, email_to_question_sub_id_map)
+    
+    def group_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        while not question.is_grouping_ready():
+            timeout = 5
+            print(f"Question grouping not ready! Retrying in {timeout} seconds.")
+            time.sleep(timeout)
+        qtype = question.data.get("type")
+        if qtype == "multiple_choice":
+            return self.group_mc_question(question, email_to_responses_map, email_to_question_sub_id_map)
+        elif qtype == "select_all":
+            return self.group_sel_all_question(question, email_to_responses_map, email_to_question_sub_id_map)
+        elif qtype in ["short_answer", "short_code_answer"]:
+            return self.group_short_ans_question(question, email_to_responses_map, email_to_question_sub_id_map)
+        elif qtype in ["long_answer", "long_code_answer"]:
+            return self.group_long_ans_question(question, email_to_responses_map, email_to_question_sub_id_map)
+        else:
+            print(f"Unsupported question type {qtype} for question {question.data}!")
+            return None
+
+
+    def group_mc_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        data = question.data
+        # This is a list of correct options from left (top) to right (bottom)
+        correct_seq = []
+        solution_options = data.get("solution", {}).get("options", [])
+        for option in data.get("options", []):
+            correct_seq.append(option in solution_options)
+
+        groups = {
+            "correct_seq": correct_seq,
+            # groups is a list of tuples where index:
+            # 0 is the name of the group
+            # 1 is the selected sequence (list of true false for selected) and
+            # 2 is the list of submission id's which selected that.
+            "groups": [],
+        }
+
+        for email, submission_ids in email_to_question_sub_id_map.items():
+            pass
+
+        return groups
+
+    def group_sel_all_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        return self.group_mc_question(question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict)
+
+    def group_short_ans_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        pass
+
+    def group_long_ans_question(self, num_id: str, question: GS_Question, email_to_responses_map: dict, email_to_question_sub_id_map: dict):
+        """
+        We will not be grouping long ans question so we will skip and return.
+        """
+        return None
+    
+    def add_rubric(self, question):
+        pass
+
+    def grade_question(self, question):
+        pass
 
 class ExamtoolOutline:
     name_region = GS_Crop_info(1, 2.4, 11.4, 100, 18.8)
